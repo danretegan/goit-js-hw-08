800f8b45f2de93d6a3d45eeb190fac25f370dd79
Dan <danretegan@yahoo.com>
2023-11-05 08:58:09 +0200

    Exercițiul 2 - video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Vimeo from '@vimeo/player';
+import _ from 'lodash';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo(iframe);
+
+const saveTimeToLocalStorage = _.throttle(time => {
+  localStorage.setItem('videoplayer-current-time', time);
+}, 1000);
+
+player.on('timeupdate', function (data) {
+  const currentTime = data.seconds;
+  saveTimeToLocalStorage(currentTime);
+});
+
+window.onload = () => {
+  const storedTime = localStorage.getItem('videoplayer-current-time');
+  if (storedTime) {
+    try {
+      player.setCurrentTime(storedTime);
+    } catch (error) {
+      switch (error.name) {
+        case 'RangeError':
+          console.log('Invalid time.');
+          break;
+        default:
+          console.log('Error occurred.');
+          break;
+      }
+    }
+  }
+};
