d73cdd62e09f443fc10b20e9d2d8925c2f1803db
Dan <danretegan@yahoo.com>
2023-11-06 10:43:07 +0200

    Exercițiul 3 - formularul pentru feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+// Obținem referințele către elementele specifice din documentul HTML astfel, ulterior, vom putea manipula aceste elemente (accesam valori introduse de utilizator sau adăugam EventListener):
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+// Definim funcția saveToLocalStorage utilizând throttle:
+const saveToLocalStorage = throttle(() => {
+  // Creăm un obiect care să reprezinte starea formularului:
+  const feedbackFormState = {
+    email: emailInput.value, // Salvam valoarea introdusa pentru adresa de email.
+    message: messageInput.value, // Salvam valoarea introdusa pentru mesaj.
+  };
+  // Salvam starea formularului în localStorage sub formă de string JSON:
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+}, 500); // Aplicăm throttle pentru a limita frecvența de apel a funcției la fiecare 500 de milisecunde.
+
+// Adăugam un ascultător pentru evenimentul 'input' care declanșează funcția saveToLocalStorage:
+form.addEventListener('input', saveToLocalStorage);
+
+// Adăugam un ascultător pentru evenimentul 'load' al ferestrei, care restaurează starea anterioară a formularului din localStorage:
+window.addEventListener('load', () => {
+  // Verificăm dacă există o stare salvată în localStorage:
+  const savedState = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (savedState) {
+    // Restaurăm valorile anterioare ale adresei de email și mesajului:
+    emailInput.value = savedState.email;
+    messageInput.value = savedState.message;
+  }
+});
+
+// Adaugăm un ascultător pentru evenimentul 'submit' al formularului:
+form.addEventListener('submit', event => {
+  // Previne comportamentul implicit al formularului:
+  event.preventDefault();
+  // Creăm un obiect care să reprezinte valorile curente ale formularului:
+  const currentValues = {
+    email: emailInput.value, // Salvăm valoarea actuală a adresei de email.
+    message: messageInput.value, // Salvăm valoarea actuală a mesajului.
+  };
+  // Afișăm valorile curente ale formularului în consolă:
+  console.log(currentValues);
+  // Înlăturăm starea formularului din localStorage:
+  localStorage.removeItem('feedback-form-state');
+});
